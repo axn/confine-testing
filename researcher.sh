44bbf865b5d500bd131418d44d5772139d3b89e3
root <root@confine-vct.pats.ua.ac.be>
2013-10-24 14:46:48 +0200

    also use improved paths in script

--- researcher.sh
+++ researcher.sh
@@ -1,33 +1,31 @@
 function start_researcher(){
 	
-	VCT_CONTAINER=$1
-	VCT_CONTAINER_URL=$2
-	VCT_IP=$3
+	RESEARCH_CONTAINER=$1
+	RESEARCH_CONTAINER_URL=$2
 
-	# fetch/copy the latest VCT container
-	if [[ ! -f $VCT_CONTAINER ]]; then
-		wget --no-check-certificate $VCT_CONTAINER_URL # TODO add more versions and loop over them
-	fi
-	if [[ ! -f $VCT_CONTAINER ]]; then
-		echo "Could not download $VCT_CONTAINER"
+	# fetch/copy the latest research container
+	# if [[ ! -f dl/$RESEARCH_CONTAINER ]]; then
+	# 	wget --no-check-certificate $RESEARCH_CONTAINER_URL -O dl/$RESEARCH_CONTAINER
+	# fi
+	if [[ ! -f dl/$RESEARCH_CONTAINER ]]; then
+		echo "Could not download $RESEARCH_CONTAINER"
 		exit 1
 	fi
 
 	echo "Unpacking..."
-	rm -rf vct_rootfs
-	mkdir -p vct_rootfs
-	tar -C vct_rootfs --numeric-owner -xJf $VCT_CONTAINER
+	tar -C researcher --numeric-owner -xJf $RESEARCH_CONTAINER
 
+	cd researcher
 	echo "Patching..."
 	rm -rf .pc # remove old quilt information, if any
 	quilt push -a -v
 
 	echo "Starting LXC..."
-	lxc-start --name vct -f vct_rootfs/vct/config -d
+	lxc-start --name researcher -f researcher_lxc/config -d
 
 	echo "Sleeping 10 seconds until booted..."
 	sleep 10
 
 	echo "Pinging..."
-	ping6 -c 1 fdf6:1e51:5f7b:b50c::2 && echo "ok"
+	ping6 -c 1 fdf6:1e51:5f7b:b50c::3 && echo "ok"
 }
