eb28c69aeb6076d71d9f4166b6654d2f26d9efe9
root <root@confine-vct.pats.ua.ac.be>
2013-10-25 15:53:08 +0200

    add SSH support

--- researcher.sh
+++ researcher.sh
@@ -22,6 +22,10 @@ function start_researcher(){
 	echo "Patching..."
 	rm -rf .pc # remove old quilt information, if any
 	quilt push -a -v
+	
+	echo "Adding key..."
+	mkdir researcher_lxc/rootfs/root/.ssh
+	cp ../sshkey/id_rsa.pub researcher_lxc/rootfs/root/.ssh/authorized_keys
 
 	echo "Starting LXC..."
 	lxc-start --name researcher -f researcher_lxc/config -s lxc.rootfs=$(pwd)/researcher_lxc/rootfs -d
@@ -31,6 +35,9 @@ function start_researcher(){
 
 	echo "Pinging..."
 	ping6 -c 1 fdf6:1e51:5f7b:b50c::3 && echo "ok"
+	
+	echo "SSHing..."
+	ssh -i ../sshkey/id_rsa -o StrictHostKeyChecking=no root@fdf6:1e51:5f7b:b50c::3 'whoami'
 	
 	cd ..
 }
