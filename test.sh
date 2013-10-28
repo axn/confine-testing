6c076cc8efc6ca83d31b5382b0d712169a5aa2e9
javonts <jeroen.avonts@ua.ac.be>
2013-10-28 12:24:40 +0100

    Added exit status of tests and archive if failed

--- test.sh
+++ test.sh
@@ -17,5 +17,15 @@ start_vct $VCT_CONTAINER $VCT_CONTAINER_URL
 source researcher.sh
 start_researcher $RESEARCH_CONTAINER $RESEARCH_CONTAINER_URL
 
+if [[ run_tests != 0 ]]; then
+    id = $(date +%Y%m%d_%H%M%S)
+    archive_vct $id
+    archive_researcher $id
+fi
+
+tear_down_researcher
+tear_down_vct
+
+
 echo "bye"
 exit 0
