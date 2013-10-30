9d34331189ca01e863100306fc3fa9156eaacb89
javonts <jeroen.avonts@ua.ac.be>
2013-10-30 09:01:52 +0100

    Correct some modifications

--- host.sh
+++ host.sh
@@ -2,13 +2,12 @@ LXC_NETWORK_LINK="vmbr"
 
 function configure_network() {
     if ! brctl show | grep $LXC_NETWORK_LINK; then
-        echo "$LXC_NETWORK_LINK does not exist, creating bridge..."
-        brctl addbr $LXC_NETWORK_LINK;
-    else
+        echo "Bridge $LXC_NETWORK_LINK does not exist, creating bridge..."
         if ip link show $LXC_NETWORK_LINK; then
             echo "Interface $LXC_NETWORK_LINK exists but is not a bridge."
             return 1;
         fi
+        brctl addbr $LXC_NETWORK_LINK;
     fi
     echo "Set $LXC_NETWORK_LINK up..."
     ip link set dev $LXC_NETWORK_LINK up;
