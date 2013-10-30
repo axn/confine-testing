4d30b46de3d75bf90b885993b16f88b7ade00dbe
javonts <jeroen.avonts@ua.ac.be>
2013-10-30 09:06:13 +0100

    Really silence output

--- host.sh
+++ host.sh
@@ -1,9 +1,9 @@
 LXC_NETWORK_LINK="vmbr"
 
 function configure_network() {
-    if ! brctl show | grep $LXC_NETWORK_LINK > /dev/null; then
+    if ! brctl show | grep $LXC_NETWORK_LINK > /dev/null 2>&1; then
         echo "Bridge $LXC_NETWORK_LINK does not exist, creating bridge..."
-        if ip link show $LXC_NETWORK_LINK > /dev/null; then
+        if ip link show $LXC_NETWORK_LINK > /dev/null 2>&1; then
             echo "Interface $LXC_NETWORK_LINK exists but is not a bridge."
             return 1;
         fi
