80d90cadc2c62d03545568a8f2fbf4a760c7dd11
javonts <jeroen.avonts@ua.ac.be>
2013-11-04 15:34:41 +0100

    Fix faulty bump

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@
 # lots of work to do
 
 # URLs
-VCT_CONTAINER=vct-container,2013080200.tar.xz
+VCT_CONTAINER=vct-container,20131104.tar.xz
 VCT_CONTAINER_URL=https://media.confine-project.eu/vct-container/$VCT_CONTAINER
-RESEARCH_CONTAINER=researcher,20131104.tar.xz
+RESEARCH_CONTAINER=researcher,20131029.tar.xz
 RESEARCH_CONTAINER_URL=https://media.confine-project.eu/researcher-container/$RESEARCH_CONTAINER
 
 set -e # fail on any exception
