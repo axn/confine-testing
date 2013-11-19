c79478005e9aa60a6598f0324b73fe3f43e50780
javonts <jeroen.avonts@ua.ac.be>
2013-11-19 10:26:36 +0100

    Updated vct-container

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 # lots of work to do
 
 # URLs
-VCT_CONTAINER=${VCT_CONTAINER:-vct-container,76e93a5efffa1ec48299af5b960e7dadc2d3e3c1,c4ccc10e10be2754f6ac1fb7be6ee87f8de267e9,76e93a5efffa1ec48299af5b960e7dadc2d3e3c1.tar.xz}
+VCT_CONTAINER=${VCT_CONTAINER:-vct-container,vctffb4d14,controllerb55b35f,nodefwffb4d14.tar.xz}
 VCT_CONTAINER_URL=https://media.confine-project.eu/vct-container/$VCT_CONTAINER
 RESEARCH_CONTAINER=${RESEARCH_CONTAINER:-researcher,20131107.tar.xz}
 RESEARCH_CONTAINER_URL=https://media.confine-project.eu/researcher-container/$RESEARCH_CONTAINER
