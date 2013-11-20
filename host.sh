3968022b38f33afa5ba0efb9876759dd198216aa
javonts <jeroen.avonts@ua.ac.be>
2013-11-20 16:30:22 +0100

    allow for separate bridge config

--- host.sh
+++ host.sh
@@ -1,5 +1,3 @@
-LXC_NETWORK_LINK="vmbr"
-
 function configure_network(){
     if ! brctl show | grep $LXC_NETWORK_LINK > /dev/null 2>&1; then
         echo "Bridge $LXC_NETWORK_LINK does not exist, creating bridge..."
@@ -12,22 +10,24 @@ function configure_network(){
     echo "Set $LXC_NETWORK_LINK up..."
     ip link set dev $LXC_NETWORK_LINK up;
     
-    if ! ip -6 addr show dev vmbr | grep fdf6:1e51:5f7b:b50c::1/64 > /dev/null 2>&1; then
+    if ! ip -6 addr show dev $LXC_NETWORK_LINK | grep ${IPPREFIX}1/64 > /dev/null 2>&1; then
         echo "Configure $LXC_NETWORK_LINK..."
-        ip -6 addr add fdf6:1e51:5f7b:b50c::1/64 dev $LXC_NETWORK_LINK
+        ip -6 addr add ${IPPREFIX}1/64 dev $LXC_NETWORK_LINK
     fi
     return 0
 }
 
 function offset_ips(){
 	IPOFFSET=${IPOFFSET:-0};
-	PREFIX="fdf6:1e51:5f7b:b50c::";
-	VCT_IP=${PREFIX}$((2+${IPOFFSET}));
-	RESEARCHER_IP=${PREFIX}$((3+${IPOFFSET}));
+	VCT_IP=${IPPREFIX}$((2+${IPOFFSET}));
+	RESEARCHER_IP=${IPPREFIX}$((3+${IPOFFSET}));
 	echo "vct container on $VCT_IP, researcher container on $RESEARCHER_IP"
 	if ! [ $IPOFFSET -eq 0 ]; then
 		echo "Updating patches to IP offset $IPOFFSET"
-		find */patches/ -type f -exec sed -i "s/${PREFIX}2/${VCT_IP}/g" {} \;
-		find */patches/ -type f -exec sed -i "s/${PREFIX}3/${RESEARCHER_IP}/g" {} \;
+		find */patches/ -type f -exec sed -i "s/fdf6:1e51:5f7b:b50c::2/${VCT_IP}/g" {} \;
+		find */patches/ -type f -exec sed -i "s/fdf6:1e51:5f7b:b50c::3/${RESEARCHER_IP}/g" {} \;
 	fi
 }
+
+LXC_NETWORK_LINK=${LXC_NETWORK_LINK:-"vmbridge"};
+IPPREFIX=${IPPREFIX:-"fdf6:1e51:5f7b:b50c::"};
