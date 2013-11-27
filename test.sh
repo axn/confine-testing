6e4a0c34ca62e69118965abf642c6ac53beb3e3f
Bart Braem <bart.braem@ua.ac.be>
2013-11-27 10:55:33 +0100

    use latest container, with spur support

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 # URLs
 VCT_CONTAINER=${VCT_CONTAINER:-vct-container,vctffb4d14,controllerb55b35f,nodefwffb4d14.tar.xz}
 VCT_CONTAINER_URL=https://media.confine-project.eu/vct-container/$VCT_CONTAINER
-RESEARCH_CONTAINER=${RESEARCH_CONTAINER:-researcher,20131107.tar.xz}
+RESEARCH_CONTAINER=${RESEARCH_CONTAINER:-researcher,20131126.tar.xz}
 RESEARCH_CONTAINER_URL=https://media.confine-project.eu/researcher-container/$RESEARCH_CONTAINER
 
 set -e # fail on any exception
