a9634426cee233bc62cdd46b81ba34b002475258
javonts <jeroen.avonts@ua.ac.be>
2013-11-27 16:06:05 +0100

    Checkout patches before modification

--- host.sh
+++ host.sh
@@ -23,6 +23,7 @@ function offset_ips(){
 	RESEARCHER_IP=${IPPREFIX}$((3+${IPOFFSET}));
 	echo "vct container on $VCT_IP, researcher container on $RESEARCHER_IP"
 	echo "Updating patches to IP offset $IPOFFSET"
+	git checkout */patches
 	find */patches/ -type f -exec sed -i "s/fdf6:1e51:5f7b:b50c::2/${VCT_IP}/g" {} \;
 	find */patches/ -type f -exec sed -i "s/fdf6:1e51:5f7b:b50c::3/${RESEARCHER_IP}/g" {} \;
 	find */patches/ -type f -exec sed -i "s/vmbridge/${LXC_NETWORK_LINK}/g" {} \;
